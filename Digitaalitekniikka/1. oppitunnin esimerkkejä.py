44d1910f08b65e4eab476a0d4a67f9c411e3657a
Teppo Toropainen <toropainenteppo@gmail.com>
2025-01-13 16:04:18 +0200

    1. oppitunti digitaalitekniikka

---
+++ Digitaalitekniikka/1. oppitunnin esimerkkejä.py
@@ -0,0 +1,11 @@
+#0 potenssi esimerkki
+print(10**0)
+print(5**0)
+
+#2 järjestelmä esimerkki
+binary_str = "101"
+decimal = int(binary_str, 2)
+print(decimal)
+
+#ACSII-koodi esimerkki
+print(ord("A"))
